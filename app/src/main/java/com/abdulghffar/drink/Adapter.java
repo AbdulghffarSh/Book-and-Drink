e9ea4f2a791f31541cffddfce8a993f57926e6e1
Abdulghffar <abdulghffar.sh@hotmail.com>
2022-05-25 22:57:52 +0300

    Book&Drink App

--- app/src/main/java/com/abdulghffar/drink/Adapter.java
+++ app/src/main/java/com/abdulghffar/drink/Adapter.java
@@ -1,13 +1,18 @@
 package com.abdulghffar.drink;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -17,6 +22,9 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     ArrayList<item> itemArrayList;
 
     public Adapter(Context context, ArrayList<item> itemArrayList) {
+
+
+
         this.context = context;
         this.itemArrayList = itemArrayList;
     }
@@ -35,10 +43,23 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
 
         item item = itemArrayList.get(position);
-
-
         holder.itemName.setText(item.getItemName());
         holder.itemPrice.setText(item.getItemPrice());
+//        holder.itemImage.setImageResource(1);
+
+        if(item.getItemPicURL()!=null && !item.getItemPicURL().isEmpty()) {
+            Picasso.get()
+                    .load(item.getItemPicURL())
+                    .placeholder(R.drawable.no_image)
+                    .error(R.drawable.no_image)
+                    // To fit image into imageView
+                    .fit()
+                    // To prevent fade animation
+                    .noFade()
+                    .into(holder.itemImage);
+        } else {
+            holder.itemImage.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.no_image));
+        }
 
 
     }
@@ -49,13 +70,14 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView itemName, itemPrice, itemDescription;
-
+        TextView itemName, itemPrice;
+        ImageView itemImage;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             itemName = itemView.findViewById(R.id.itemName);
             itemPrice = itemView.findViewById(R.id.itemPrice);
+            itemImage = itemView.findViewById(R.id.img);
 
         }
     }
