194ffb10fc55655ce32a459f96aefff49835d807
Abdulghffar <abdulghffar.sh@hotmail.com>
2022-05-27 21:47:53 +0300

    Book&Drink App

--- app/src/main/java/com/abdulghffar/drink/profileFragment.java
+++ app/src/main/java/com/abdulghffar/drink/profileFragment.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class profileFragment extends Fragment {
 View view;
     @Override
@@ -15,6 +18,25 @@ View view;
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
         return view;
     }
+
+
+    public void getUserData(){
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+
+            user
+
+        } else {
+            // No user is signed in
+        }
+
+
+
+    }
+
 }
